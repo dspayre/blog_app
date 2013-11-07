2f320fa35ad44c8dd104fd8fcc7209f7c0b1f1f6
bchau <bruce@brucechau.com>
2013-11-07 15:23:06 -0500

    change remote server dns

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :branch, "master"
 set :use_sudo, true
 
 
-server "rubydemo001.cloudapp.net", :web, :app, :db, primary: true
+server "rubydemo002.cloudapp.net", :web, :app, :db, primary: true
 
 
 set :deploy_to, "/home/#{user}/apps/#{application}"
